770b00de6235b6078c743c1062c8acfcca9eeaef
AlkenD <desteralken@gmail.com>
2023-07-22 14:16:17 +0530

    fix(svelte): folder structure remove(ui): removed static css classes

---
+++ packages/svelte/src/lib/components/Slider.svelte
@@ -0,0 +1,7 @@
+<div class="absolute bottom-0 h-fit max-w-2xl w-full mx-auto left-0 right-0 p-4">
+	<div class="relative rounded-full overflow-hidden w-full h-1 hover:h-2 transition-[height]">
+		<div class="absolute w-full h-full bg-blue-500" />
+		<div class="absolute w-full h-full bg-blue-500" />
+		<div class="absolute w-full h-full bg-blue-500" />
+	</div>
+</div>
