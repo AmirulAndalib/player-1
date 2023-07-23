c15c10bd4b85a465b72747a5d004d6b611d06895
AlkenD <desteralken@gmail.com>
2023-07-23 16:50:25 +0530

    docs(layout): improved homepage and images

--- apps/docs/src/examples/SveltePlayer.svelte
+++ apps/docs/src/examples/SveltePlayer.svelte
@@ -2,6 +2,6 @@
   import { AwePlayer } from "@awe-player/svelte";
 </script>
 
-<div class="max-w-4xl mx-auto aspect-video">
+<div class="max-w-4xl mx-auto aspect-video rounded-md overflow-hidden">
   <AwePlayer type="lite" />
 </div>
